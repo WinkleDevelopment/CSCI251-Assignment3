50715061bc2ffa8f9a830c7d3f74676057fe1ecf
xWinkleAU <gamingkyle7@outlook.com>
2021-05-31 20:14:58 +1000

    Updated files

--- Assignment3/Monitor.cpp
+++ Assignment3/Monitor.cpp
@@ -10,12 +10,99 @@
 
 #include "Monitor.h"
 
-int main() {
-	
-	
-	
-	
-	
+/* User Class */
+User::User() {
+	vector<string> tmp;
+
+	this->name = "";
+	this->age = NULL;
+	this->gender = "";
+	this->history = tmp;
+}
+
+User::User(string nM, int aG, string gD) {
+	vector<string> tmp;
+
+	this->name = nM;
+	this->age = aG;
+	this->gender = gD;
+	this->history = tmp;
+}
+
+User::~User() {}
+
+User::User(const User& copyUser) {
+	this->name = copyUser.name;
+	this->age = copyUser.age;
+	this->gender = copyUser.gender;
+	this->history = copyUser.history;
+}
+
+string User::getName() { return this->name; }
+
+int User::getAge() { return this->age; }
+
+string User::getGender() { return this->gender; }
+
+vector<string> User::getHistory() { return this->history; }
+
+void User::setName(string newName) { this->name = newName; }
+
+void User::setAge(int newAge) { this->age = newAge; }
+
+void User::setGender(string newGender) { this->gender = newGender; }
+
+void User::setHistory(vector<string> newHistory) { this->history = newHistory; }
+
+int User::difference() {}
+
+int User::varity() {}
+
+int User::compareTo(User otherUser) {}
+
+/* Pool Class */
+Pool::Pool() {}
+
+Pool::Pool(string nM) {
+	vector<User> tmp;
+
+	this->name = nM;
+	this->users = tmp;
+}
+
+Pool::Pool(string nM, vector<User> uR) {
+	this->name = nM;
+	this->users = uR;
+}
+
+Pool::~Pool() {}
+
+Pool::Pool(const Pool& copyPool) {
+	this->name = copyPool.name;
+	this->users = copyPool.users;
+}
+
+string Pool::getName() {}
+
+vector<User> Pool::getUsers() {}
+
+void Pool::setName(string newName) {}
+
+void Pool::setUsers(vector<User> newUsers) {}
+
+void Pool::addUser(User user) {}
+
+void Pool::removeUser(User user) {}
+
+void Pool::display() {}
+
+void Pool::minimumVariety() {}
+
+void Pool::minimumDifference() {}
+
+
+/* Main */
+int main() {	
 	
 	return 0;
 }
--- Assignment3/Monitor.h
+++ Assignment3/Monitor.h
@@ -15,7 +15,27 @@
 using namespace std;
 
 class User {
-
+private:
+	string name;
+	int age;
+	string gender;
+	vector<string> history;
+public:
+	User();
+	User(string, int, string);
+	~User();
+	User(const User& copyUser);
+	string getName();
+	int getAge();
+	string getGender();
+	vector<string> getHistory();
+	void setName(string newName);
+	void setAge(int newAge);
+	void setGender(string newGender);
+	void setHistory(vector<string> newHistory);
+	int difference();
+	int varity();
+	int compareTo(User otherUser);
 };
 
 class Pool {
@@ -27,5 +47,14 @@ public:
 	Pool(string);
 	Pool(string, vector<User>);
 	~Pool();
-
+	Pool(const Pool& copyPool);
+	string getName();
+	vector<User> getUsers();
+	void setName(string newName);
+	void setUsers(vector<User> newUsers);
+	void addUser(User user);
+	void removeUser(User user);
+	void display();
+	void minimumVariety();
+	void minimumDifference();
 };
